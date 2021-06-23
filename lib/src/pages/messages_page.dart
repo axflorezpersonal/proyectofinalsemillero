1621230eaa84ea344160c2518a83face3e77f6b0
axflorezpersonal <axflorez@personalsoft.com.co>
2021-06-23 11:39:29 -0500

    se agrega pagina de mensajes

---
+++ lib/src/pages/messages_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class MessagesPage extends StatefulWidget {
+  MessagesPage({Key? key}) : super(key: key);
+
+  @override
+  _MessagesPageState createState() => _MessagesPageState();
+}
+
+class ChatMessage {
+  String messageContent;
+  String messageType;
+  ChatMessage({required this.messageContent, required this.messageType});
+}
+
+class _MessagesPageState extends State<MessagesPage> {
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+        messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Is there any thing wrong?", messageType: "sender"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Messages')),
+      body: Column(
+        children: [
+          Expanded(child: listMessage()),
+          Container(padding: EdgeInsets.only(top:30,left:10,right:10,bottom:30), child: sectionbuttons()),
+        ],
+      ),
+    );
+  }
+
+  sectionbuttons(){
+   return  Row(
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: (){
+                    },
+                    child: Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Icon(Icons.add, color: Colors.white, size: 20, ),
+                    ),
+                  ),
+                  SizedBox(width: 15,),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Write message...",
+                        hintStyle: TextStyle(color: Colors.black54),
+                        border: InputBorder.none
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 15,),
+                  FloatingActionButton(
+                    onPressed: (){},
+                    child: Icon(Icons.send,color: Colors.white,size: 18,),
+                    backgroundColor: Colors.blue,
+                    elevation: 0,
+                  ),
+                ],
+                
+              );
+  }
+
+  Widget listMessage() {
+    return ListView.builder(
+      itemCount: messages.length,
+      shrinkWrap: true,
+      padding: EdgeInsets.only(top: 10, bottom: 10),
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        return Container(
+          padding: EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+          child: Align(
+            alignment: (messages[index].messageType == "receiver"
+                ? Alignment.topLeft
+                : Alignment.topRight),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: (messages[index].messageType == "receiver"
+                    ? Colors.grey.shade200
+                    : Colors.blue[200]),
+              ),
+              padding: EdgeInsets.all(16),
+              child: Text(
+                messages[index].messageContent,
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
